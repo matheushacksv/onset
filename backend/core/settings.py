5005ed5413e844ab2da82b14b0d48dc5b0f10f57
Matheus Hack <matheushacksv@gmail.com>
2026-05-07 15:58:50 -0300

    fix: env_file docker-compose and email in settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -169,3 +169,13 @@ Q_CLUSTER = {
     'retry': 120,
     'orm': 'default'
 }
+
+# Email
+
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
